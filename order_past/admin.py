8c202204ca203b91d9194684ed25b8c8d7811d10
Khouloud <khouloudfrikha@gmail.com>
2023-03-16 15:42:16 +0100

    add count

--- order_past/admin.py
+++ order_past/admin.py
@@ -1,3 +1,17 @@
+
 from django.contrib import admin
+from order_past.models import Order_files
+from order_past.models import Order_past_per_divsion
+from order_past.models import Order_past_per_organisme
+from order_past.models import Order_past_per_cp
+from order_past.models import Order_past_per_errors
+
+
 
 # Register your models here.
+admin.site.register(Order_files)
+admin.site.register(Order_past_per_divsion)
+admin.site.register(Order_past_per_organisme)
+admin.site.register(Order_past_per_cp)
+admin.site.register(Order_past_per_errors)
+
--- order_past/urls.py
+++ order_past/urls.py
@@ -2,4 +2,7 @@ from django.urls import path, include
 from order_past import views
 urlpatterns = [
     path('home/',views.home,name='order_past_home'),
+    path('calcul/', views.upload_files,name='Ordercalcul'),
+
 ]
+
